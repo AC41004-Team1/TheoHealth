30f5476caa40086f23bff35bfb7453f24f4dacf5
jessechan23 <jessechan10@gmail.com>
2021-09-30 16:23:56 +0100

    Added all 4 sensor data onto one graph
    Also added some test 3js stuff

--- Software_Code/visualize.php
+++ Software_Code/visualize.php
@@ -25,21 +25,21 @@ $connection = openCon();
   <?php
     try{
 
-      //  $sql = "SELECT * FROM TABLENAME";
-        $queryInput = "CALL getAllClientData(1)";
+      // Change first para to client thing;
+        $queryInput = "CALL getClientDataWithSensor(1,1)";
         $result = $connection->query($queryInput);
         echo "Done Query";
         echo "End of result";
         if($result->num_rows > 0){
           echo "Done if";
-          $time = array();
-          $reading = array();
+          $time1 = array();
+          $reading1 = array();
           echo "Created variables";
           while($row = $result->fetch_object()){
 
-            $time[] = $row->TimeStamp;
+            $time1[] = $row->TimeStamp;
 
-            $reading[] = $row->Reading;
+            $reading1[] = $row->Reading;
 
           }
           echo "populated arrays";
@@ -52,9 +52,112 @@ $connection = openCon();
         die($e->getMessage());
       }
 
-      unset($conn);
+        closeCon($connection);
+        $connection = openCon();
 
-    ?>
+
+
+      try{
+
+        // Change first para to client thing;
+          $queryInput = "CALL getClientDataWithSensor(1,2)";
+          $result = $connection->query($queryInput);
+          echo "Done Query";
+          echo "End of result";
+          if($result->num_rows > 0){
+            echo "Done if";
+            $time2 = array();
+            $reading2 = array();
+            echo "Created variables";
+            while($row = $result->fetch_object()){
+
+              $time2[] = $row->TimeStamp;
+
+              $reading2[] = $row->Reading;
+
+            }
+            echo "populated arrays";
+            unset($result);
+            } else {
+              echo"Problem2";
+            }
+          }catch(PDOException $e){
+            echo "no result";
+          die($e->getMessage());
+        }
+
+        closeCon($connection);
+        $connection = openCon();
+
+
+
+      try{
+
+        // Change first para to client thing;
+          $queryInput = "CALL getClientDataWithSensor(1,3)";
+          $result = $connection->query($queryInput);
+          echo "Done Query";
+          echo "End of result";
+          if($result->num_rows > 0){
+            echo "Done if";
+            $time3 = array();
+            $reading3 = array();
+            echo "Created variables";
+            while($row = $result->fetch_object()){
+
+              $time3[] = $row->TimeStamp;
+
+              $reading3[] = $row->Reading;
+
+            }
+            echo "populated arrays";
+            unset($result);
+            } else {
+              echo"Problem2";
+            }
+          }catch(PDOException $e){
+            echo "no result";
+          die($e->getMessage());
+        }
+        closeCon($connection);
+        $connection = openCon();
+
+
+
+      try{
+
+        // Change first para to client thing;
+          $queryInput = "CALL getClientDataWithSensor(1,4)";
+          $result = $connection->query($queryInput);
+          echo "Done Query";
+          echo "End of result";
+          if($result->num_rows > 0){
+            echo "Done if";
+            $time4 = array();
+            $reading4 = array();
+            echo "Created variables";
+            while($row = $result->fetch_object()){
+
+              $time4[] = $row->TimeStamp;
+
+              $reading4[] = $row->Reading;
+
+            }
+            echo "populated arrays";
+            unset($result);
+            } else {
+              echo"Problem2";
+            }
+          }catch(PDOException $e){
+            echo "no result";
+          die($e->getMessage());
+        }
+
+
+
+
+      ?>
+
 
 
     <!--
@@ -75,15 +178,15 @@ $connection = openCon();
 
 <h1> Bee doop 1 </h1>
 
-    <div style = "width: 50%">
-      <canvas id="myChart"></canvas>
+    <div style = "width: 100%">
+       <canvas id="myChart"></canvas>
     </div>
 <h1> Hello there team 1 </h1>
   </body>
 
 
 
-  <canvas id="myChart" width="400" height="400"></canvas>
+  <!-- <canvas id="myChart" width="400" height="400"></canvas> -->
   <script>
 
 
@@ -121,23 +224,33 @@ $connection = openCon();
 
 
 // setup
-const time = <?php echo json_encode($time); ?>;
-console.log(time);
-const reading = <?php echo json_encode($reading); ?>;
-console.log(reading);
+var time1 = <?php echo json_encode($time1); ?>;
+time1.map((e)=>{return new Date(e)});
+
+
+
+
+console.log(time1);
+
+const reading1 = <?php echo json_encode($reading1); ?>;
+const reading2 = <?php echo json_encode($reading2); ?>;
+const reading3 = <?php echo json_encode($reading3); ?>;
+const reading4 = <?php echo json_encode($reading4); ?>;
+
+console.log(reading1);
 
 const data = {
-labels: [0,1,2,3,4,5,6,7,8,9,1,1,1,1,,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], // Put times here but we need to edit it Manually put in time labels to fix this problem
+labels: time1, // Put times here but we need to edit it Manually put in time labels to fix this problem
 datasets: [{
-    label: 'Muscle Tension Sensor 1',
-    data: reading, // Replace with Sensor
+    label: 'Muscle Tension Left Hamstring (Semitendinosus)',
+    data: reading1, // Replace with Sensor
     backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 199, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
+        'rgba(255, 206, 186, 0.2)',
         'rgba(75, 192, 192, 0.2)',
         'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
+        'rgba(255, 159, 164, 0.2)'
     ],
     borderColor: [
         'rgba(255, 99, 132, 1)',
@@ -147,8 +260,72 @@ datasets: [{
         'rgba(153, 102, 255, 1)',
         'rgba(255, 159, 64, 1)'
     ],
-    borderWidth: 0.5
+    borderWidth: 0.1,
+},
+{
+    label: 'Right Hamstring (Semitendinosus)',
+    data: reading2, // Replace with Sensor
+    backgroundColor: [
+        'rgba(25, 99, 132, 0.2)',
+        'rgba(54, 62, 235, 0.2)',
+        'rgba(25, 206, 86, 0.2)',
+        'rgba(75, 12, 192, 0.2)',
+        'rgba(13, 102, 5, 0.2)',
+        'rgba(25, 59, 64, 0.2)'
+    ],
+    borderColor: [
+        'rgba(55, 99, 132, 1)',
+        'rgba(54, 162, 35, 1)',
+        'rgba(255, 26, 86, 1)',
+        'rgba(75, 12, 192, 1)',
+        'rgba(153, 12, 25, 1)',
+        'rgba(25, 19, 64, 1)'
+    ],
+    borderWidth: 0.1
+  },
+  {
+      label: 'Left Quadricep (Rectus Femoris)',
+      data: reading3, // Replace with Sensor
+      backgroundColor: [
+          'rgba(25, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(5, 206, 86, 0.2)',
+          'rgba(75, 92, 192, 0.2)',
+          'rgba(13, 102, 25, 0.2)',
+          'rgba(5, 159, 6, 0.2)'
+      ],
+      borderColor: [
+          'rgba(55, 99, 132, 1)',
+          'rgba(54, 12, 35, 1)',
+          'rgba(255, 26, 86, 1)',
+          'rgba(75, 12, 12, 1)',
+          'rgba(153, 12, 255, 1)',
+          'rgba(25, 159, 64, 1)'
+      ],
+      borderWidth: 0.1
+    },
+    {
+        label: 'Right Quadricep (Rectus Femoris)',
+        data: reading3, // Replace with Sensor
+        backgroundColor: [
+            'rgba(25, 99, 132, 0.2)',
+            'rgba(54, 162, 35, 0.2)',
+            'rgba(25, 206, 86, 0.2)',
+            'rgba(75, 12, 192, 0.2)',
+            'rgba(132, 102, 25, 0.2)',
+            'rgba(215, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(155, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 26, 86, 1)',
+            'rgba(175, 12, 192, 1)',
+            'rgba(153, 12, 255, 1)',
+            'rgba(252, 19, 64, 1)'
+        ],
+        borderWidth: 0.1
 }]
+
 };
 
 
@@ -159,6 +336,9 @@ type: 'line',
 data,
 options: {
     scales: {
+      xAxes: [{
+        type: 'time'
+      }],
         y: {
             beginAtZero: true
         }
@@ -176,4 +356,40 @@ config
   closeCon($connection);
 ?>
   </script>
+
+  <script type="module">
+
+  // Find the latest version by visiting https://cdn.skypack.dev/three.
+
+  import * as THREE from 'https://cdn.skypack.dev/three@0.132.2';
+
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth/window.innerHeight,
+    0.1,
+    1000
+  );
+  const renderer = new THREE.WebGLRenderer({antialias: true});
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+const material = new THREE.MeshBasicMaterial( {color: 0x0000ff} );
+const cube = new THREE.Mesh( geometry, material );
+scene.add(cube);
+
+  camera.position.z = 5;
+
+  function animate(){
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    requestAnimationFrame(animate);
+    renderer.render(scene,camera);
+  }
+  animate();
+</script>
 </html>
