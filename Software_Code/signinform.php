38a8c40dc90b32489d8a98cd5cf7972e1222cc18
GriffinKF <tkashaf@dundee.ac.uk>
2021-10-05 18:37:37 +0100

    Global session variable userInfoArray now contains Username, index, and role in array positions 0, 1 and 2 respectively

--- Software_Code/signinform.php
+++ Software_Code/signinform.php
@@ -6,12 +6,12 @@
   
   // Check if the form is submitted
     if (isset($_POST['submit'])) {
-      // retrieve the form data by using the element's name attributes value as key
+      //retrieve the form data by using the element's name attributes value as key
       $username = $_POST['username'];
-      // $password = $_POST['password'];
+      //$password = $_POST['password'];
       $password = hash('ripemd160', $_POST['password']);
-      // display the results
-      //echo '<h3>Form POST Method</h3>'; echo 'Your email is ' . $username . ' and your password is ' . $password;
+      //display the results
+      //echo '<h3>Form POST Method</h3>'; echo 'Your email is ' . $username . ' and your hashed password is ' . $password;
   }
 
   $usernameQ = "SELECT * FROM login WHERE Username = '$username'";
@@ -27,7 +27,8 @@
     $resultRow = $result->fetch_array();
       if ($resultRow['UserPassword'] == $password) {
         //Successfull login
-        $_SESSION["UserInfo"] = $resultRow;
+        $userInfoArray=array($resultRow['Username'], $resultRow['UserIndex'], $resultRow['Role']);
+        $_SESSION["userInfoArray"] = $userInfoArray;
         header("Location: dashboard.php");
       } else {
         //Failed login due to password not correct
